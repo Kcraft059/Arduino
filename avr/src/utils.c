4ed437dca4a72e675c95c4d9d06c40d22d0998c5
Kcraft059 <kcraft059.msg@gmail.com>
2026-05-16 14:11:40 +0200

    malloc/free wip

--- avr/src/utils.c
+++ avr/src/utils.c
@@ -4,13 +4,44 @@
 
 uint8_t mask = (1 << PORTB5);
 
+struct __attribute((packed)) mlc_header {
+  uint8_t sizel;
+  uint8_t offsetl;
+  uint8_t sizeh_offseth;
+};
+
+extern uint8_t alc_flags;
 extern void* malloc(uint16_t);
+extern struct mlc_header malloc_get_header(void*);
 
 void blink_bin(uint16_t inp);
+void blink_iter(uint16_t itr);
 void fast_blink();
 
 void c_entry() {
-  blink_bin((uint16_t)malloc(255));
+  uint16_t* addr = malloc(4);
+  uint16_t* addr2 = malloc(3);
+  
+  if (addr != 0) {
+    *addr = 1;
+    struct mlc_header hdr = malloc_get_header(addr);
+    blink_iter(*addr);
+    _delay_ms(3000);
+    blink_iter((uint16_t)(hdr.sizel | ((hdr.sizeh_offseth & 0xf) << 8)));
+    _delay_ms(3000);
+    blink_bin((uint16_t)(hdr.offsetl | ((hdr.sizeh_offseth >> 4) << 8)));
+  }
+  _delay_ms(5000);
+  
+  if (addr2 != 0) {
+    *addr2 = 2;
+    struct mlc_header hdr2 = malloc_get_header(addr2);
+    blink_iter(*addr2);
+    _delay_ms(3000);
+    blink_iter((uint16_t)(hdr2.sizel | ((hdr2.sizeh_offseth & 0xf) << 8)));
+    _delay_ms(3000);
+    blink_bin((uint16_t)(hdr2.offsetl | ((hdr2.sizeh_offseth >> 4) << 8)));
+  }
 }
 
 void fast_blink() {
@@ -19,6 +50,12 @@ void fast_blink() {
   PORTB |= mask;
   _delay_ms(100);
   PORTB &= ~mask;
+}
+
+void blink_iter(uint16_t itr) {
+  for (; itr > 0; itr--) {
+    fast_blink();
+  }
 }
 
 void blink_bin(uint16_t inp) {
