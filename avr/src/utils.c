e3e223593d56283cb14016c68804939f8801ba64
Kcraft059 <kcraft059.msg@gmail.com>
2026-05-20 07:56:34 +0200

    wip

--- avr/src/utils.c
+++ avr/src/utils.c
@@ -1,5 +1,8 @@
+#include "stdlib.h"
 #include <avr/io.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 #include <util/delay.h>
 
 uint8_t mask = (1 << PORTB5);
@@ -14,34 +17,44 @@ extern uint8_t alc_flags;
 extern void* malloc(uint16_t);
 extern struct mlc_header malloc_get_header(void*);
 
+void usart_send(char chr);
+void usart_init(uint16_t ubrr);
 void blink_bin(uint16_t inp);
 void blink_iter(uint16_t itr);
 void fast_blink();
 
 void c_entry() {
-  uint16_t* addr = malloc(4);
-  uint16_t* addr2 = malloc(3);
-  
-  if (addr != 0) {
-    *addr = 1;
-    struct mlc_header hdr = malloc_get_header(addr);
-    blink_iter(*addr);
-    _delay_ms(3000);
-    blink_iter((uint16_t)(hdr.sizel | ((hdr.sizeh_offseth & 0xf) << 8)));
-    _delay_ms(3000);
-    blink_bin((uint16_t)(hdr.offsetl | ((hdr.sizeh_offseth >> 4) << 8)));
-  }
-  _delay_ms(5000);
-  
-  if (addr2 != 0) {
-    *addr2 = 2;
-    struct mlc_header hdr2 = malloc_get_header(addr2);
-    blink_iter(*addr2);
-    _delay_ms(3000);
-    blink_iter((uint16_t)(hdr2.sizel | ((hdr2.sizeh_offseth & 0xf) << 8)));
-    _delay_ms(3000);
-    blink_bin((uint16_t)(hdr2.offsetl | ((hdr2.sizeh_offseth >> 4) << 8)));
-  }
+  usart_init(9600);
+  char* str = malloc(407);
+  char* str_ofst = str + 5;
+  memcpy(str, "size:", 5);
+
+  struct mlc_header hdr = malloc_get_header(str);
+  uint16_t size = hdr.sizel | ((hdr.sizeh_offseth & 0xf) << 8);
+
+  utoa(size, str_ofst, 10);
+  for (uint8_t i = 0; str[i] != '\0'; i++)
+    usart_send(str[i]);
+}
+
+void usart_init(uint16_t baud) {
+  // See manual -> Table 20-1
+  // Set baud rate
+  uint16_t ubrr = (F_CPU / (16ul * baud)) - 1;
+
+  // Write baud rate
+  UBRR0H = (ubrr >> 8) & 0b1111;
+  UBRR0L = ubrr;
+  /*Enable receiver and transmitter */
+  UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+  /* Set frame format: 8data, 1stop bit */
+  UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+}
+
+void usart_send(char chr) {
+  while (!(UCSR0A & (1 << UDRE0)))
+    ;
+  UDR0 = chr;
 }
 
 void fast_blink() {
